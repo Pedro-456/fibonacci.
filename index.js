3f39bf3bc7bdd976470c13d503b8c5db897840e6
Pedro-456 <pedro.etec.botoso@gmail.com>
2025-05-21 17:54:05 +0000

    fibonacci

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'hbs');
+app.set('views', './views');
+
+app.get('/', (request, response) => {
+    let resultado = [];
+    let n1 = 0, n2 = 1;
+
+    for(let i = 0; i < 20; i++) {
+        resultado.push(n1);
+        let temp = n1 + n2;
+        n1 = n2;
+        n2 = temp;
+    }
+
+    response.render('home', { valores: resultado });
+});
+
+app.listen(3000, () => {
+    console.log('Servidor na porta 3000');
+});
+
+
+
